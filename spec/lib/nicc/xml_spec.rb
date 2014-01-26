668501f8aa03b06868146c1dcc6c976a4995dc83
mgi166 <skskoari@gmail.com>
2014-01-26 10:29:40 +0900

    * Add new gem 'ActiveSupport'    * For using Hash#from_xml method * modify sample xml    * strip any long descriptions

--- spec/lib/nicc/xml_spec.rb
+++ spec/lib/nicc/xml_spec.rb
@@ -8,45 +8,35 @@ describe Nicc::XML do
     subject { nicc.parse }
     let(:nicc) { described_class.new(xml) }
 
-    describe '' do
+    describe 'Actual data(from ext api)' do
       let(:xml)  { File.read(File.join('spec', 'data', 'xml', 'sample01.xml')) }
-      it { subject }
+      its(:keys) { should == ['nicovideo_thumb_response'] }
+
+      describe 'in "nicovideo_thumb_response"' do
+        subject { nicc.parse['nicovideo_thumb_response'] }
+
+        its(['thumb']) { should include 'video_id'       => 'sm3393520' }
+        its(['thumb']) { should include 'title'          => '【アイドルマスター＋麻雀】im@s 雀姫伝　第一話 前編' }
+        its(['thumb']) { should include 'description'    => '「何故プレイ動画にしなかったのか」と何度も...' }
+        its(['thumb']) { should include 'thumbnail_url'  => 'http://tn-skr1.smilevideo.jp/smile?i=3393520' }
+        its(['thumb']) { should include 'first_retrieve' => '2008-05-22T07:23:27+09:00' }
+        its(['thumb']) { should include 'length'         => '11:07' }
+        its(['thumb']) { should include 'movie_type'     => 'mp4' }
+        its(['thumb']) { should include 'size_high'      => '39289914' }
+        its(['thumb']) { should include 'size_low'       => '33276193' }
+        its(['thumb']) { should include 'view_counter'   => '424401' }
+        its(['thumb']) { should include 'comment_num'    => '7948' }
+        its(['thumb']) { should include 'mylist_counter' => '10124' }
+        its(['thumb']) { should include 'last_res_body'  => '赤木はるかwwww 今の...' }
+        its(['thumb']) { should include 'watch_url'      => 'http://www.nicovideo.jp/watch/sm3393520' }
+        its(['thumb']) { should include 'thumb_type'     => 'video' }
+        its(['thumb']) { should include 'embeddable'     => '1' }
+        its(['thumb']) { should include 'no_live_play'   => '0' }
+        its(['thumb']) { should include 'tags'           => {'domain'=>'jp', 'tag'=>['アイドルマスター', 'アイマス雀荘リンク', 'im@s架空戦記シリーズ', 'iM@s雀姫伝', 'RAP_']} }
+        its(['thumb']) { should include 'user_id'        => '43787' }
+        its(['thumb']) { should include 'user_nickname'  => 'RAP' }
+        its(['thumb']) { should include 'user_icon_url'  => 'http://usericon.nimg.jp/usericon/s/4/43787.jpg?1389728124' }
+      end
     end
-  end
-
-  describe '#parse_children' do
-    subject { nicc.parse_children(element) }
-    let(:element) { Nokogiri::XML::DocumentFragment.parse(xml) }
-
-    describe 'the case that xml has children' do
-      let(:xml) {
-        <<-XML
-          <tags domain="jp">
-            <tag category="1" lock="1">アイドルマスター</tag>
-            <tag lock="1">アイマス雀荘リンク</tag>
-            <tag lock="1">RAP_</tag>
-          </tags>
-        XML
-      }
-
-      its(['tags']) { should include('tag' => ['アイドルマスター', 'アイマス雀荘リンク', 'RAP_']) }
-    end
-
-    describe 'the case that xml has no children' do
-      let(:xml) {
-        <<-XML
-          <tag domain="jp">rspec</tag>
-        XML
-      }
-      its(['tag']) { should == ['rspec'] }
-    end
-
-    describe 'the case that xml has nested chilren' do
-      it { pending }
-    end
-  end
-
-  describe 'parse_element' do
-    it { pending }
   end
 end
