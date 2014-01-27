8aeb70d3aaaeb79fb66cccbfe44b19046ce4efdb
mgi166 <skskoari@gmail.com>
2014-01-27 10:14:55 +0900

    * create new method 'default_merge'    * reverse_merge two hash

--- lib/nicc/response.rb
+++ lib/nicc/response.rb
@@ -1,6 +1,20 @@
+require 'tapp'
+require 'active_support/core_ext'
+
 module Nicc
   class Response
-    def initialize
+
+    # "merge" is the tentative name
+    attr_accessor :ext_hash, :i_hash, :merge
+    def initialize(ext_hash, i_hash, options)
+      @ext_hash = ext_hash
+      @i_hash   = i_hash
+      @options  = {}.merge(options)
+    end
+
+    def default_merge
+      @merge = i_hash['nicovideo_video_response']['video_info']['video'].
+        reverse_merge(ext_hash['nicovideo_thumb_response']['thumb'])
     end
   end
 end
