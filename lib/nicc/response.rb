4f6de85413692848ca6373ef1135e825ddf69d5c
mgi166 <skskoari@gmail.com>
2014-01-29 10:03:51 +0900

    * Add test 'reverse_merge' method * For using "shared_context", it shared test commonly elements

--- lib/nicc/response.rb
+++ lib/nicc/response.rb
@@ -4,13 +4,11 @@ require 'active_support/core_ext'
 module Nicc
   class Response
 
-    # "merge" is the tentative name
     attr_accessor :ext_hash, :i_hash, :merge
     def initialize(ext_hash, i_hash, options)
       @ext_hash = ext_hash
       @i_hash   = i_hash
-      @merge    = {}
-      @options  = {:thread_type => 'flat'}.merge(options)
+      @options  = { :thread_type => 'flat' }.merge(options)
     end
 
     def default_merge
@@ -29,23 +27,32 @@ module Nicc
             .reverse_merge(ext_hash['nicovideo_thumb_response']['thumb'])
             .delete_if {|key| key == 'id'}
           merge[:thread] = i_hash['nicovideo_video_response']['video_info']['thread']
+            .tap do |x|
+            x['thread_id'] = x.delete('id')
+          end
         end
       end
     end
 
-    # TODO: test & implementation
     def reverse_merge
       case @options[:thread_type]
-      when 'layer'
-        @merge.tap do |merge|
-          merge[:info] = i_hash['nicovideo_video_response']['video_info']['video']
-            .deep_merge(ext_hash['nicovideo_thumb_response']['thumb'])
-        end
       when 'flat'
-        @merge.tap do |merge|
+        @merge = ext_hash['nicovideo_thumb_response']['thumb']
+          .reverse_merge(i_hash['nicovideo_video_response']['video_info']['video'])
+          .merge(i_hash['nicovideo_video_response']['video_info']['thread'])
+          .tap do |x|
+          # key 'id' is commonly used, so change key name 'thread_id'
+          x['thread_id'] = x.delete('id')
+        end
+      when 'layer'
+        @merge = {}.tap do |merge|
           merge[:info] = ext_hash['nicovideo_thumb_response']['thumb']
             .reverse_merge(i_hash['nicovideo_video_response']['video_info']['video'])
-            .merge(i_hash['nicovideo_video_response']['video_info']['thread'])
+            .delete_if {|key| key == 'id'}
+          merge[:thread] = i_hash['nicovideo_video_response']['video_info']['thread']
+            .tap do |x|
+            x['thread_id'] = x.delete('id')
+          end
         end
       end
     end
--- spec/lib/nicc/response_spec.rb
+++ spec/lib/nicc/response_spec.rb
@@ -1,115 +1,124 @@
 # -*- coding: utf-8 -*-
 require 'spec_helper'
 
+shared_context 'The test of common elements' do
+  its(['video_id'])                    { should == 'sm3393520' }
+  its(['title'])                       { should == '【アイドルマスター＋麻雀】im@s 雀姫伝　第一話 前編' }
+  its(['thumbnail_url'])               { should == 'http://tn-skr1.smilevideo.jp/smile?i=3393520' }
+  its(['first_retrieve'])              { should == '2008-05-22T07:23:27+09:00' }
+  its(['length'])                      { should == '11:07' }
+  its(['movie_type'])                  { should == 'mp4' }
+  its(['size_high'])                   { should == '39289914' }
+  its(['size_low'])                    { should == '33276193' }
+  its(['comment_num'])                 { should == '7948' }
+  its(['last_res_body'])               { should == '赤木はるかwwww 今の...' }
+  its(['watch_url'])                   { should == 'http://www.nicovideo.jp/watch/sm3393520' }
+  its(['thumb_type'])                  { should == 'video' }
+  its(['embeddable'])                  { should == '1' }
+  its(['no_live_play'])                { should == '0' }
+  its(['tags'])                        { should == {'domain'=>'jp', 'tag'=>['アイドルマスター', 'アイマス雀荘リンク', 'im@s架空戦記シリーズ', 'iM@s雀姫伝', 'RAP_']} }
+  its(['user_id'])                     { should == '43787' }
+  its(['user_nickname'])               { should == 'RAP' }
+  its(['user_icon_url'])               { should == 'http://usericon.nimg.jp/usericon/s/4/43787.jpg?1389728124' }
+  its(['deleted'])                     { should == '0' }
+  its(['length_in_seconds'])           { should == '667' }
+  its(['upload_time'])                 { should == nil }
+  its(['default_thread'])              { should == '1211408607' }
+  its(['option_flag_ichiba'])          { should == '0' }
+  its(['option_flag_community'])       { should == '0' }
+  its(['option_flag_domestic'])        { should == '0' }
+  its(['option_flag_comment_type'])    { should == '0' }
+  its(['option_flag_adult'])           { should == '0' }
+  its(['option_flag_mobile'])          { should == '0' }
+  its(['option_flag_economy_mp4'])     { should == '1' }
+  its(['option_flag_middle_video'])    { should == '0' }
+  its(['option_flag_mobile_ng_apple']) { should == '0' }
+end
+
+shared_context 'The test of common thread elements' do
+  its(['public'])       { should == '1' }
+  its(['num_res'])      { should == '7948' }
+  its(['community_id']) { should == nil }
+  its(['thread_id'])    { should == '1211408607' }
+end
+
 describe Nicc::Response do
   let(:response) { described_class.new(ext_hash, i_hash, options) }
 
+  let(:ext_xml)  { File.read(File.join('spec', 'data', 'xml', 'sample01.xml')) }
+  let(:ext_hash) { Nicc::XML.new(ext_xml).parse }
+
+  let(:i_xml)  { File.read(File.join('spec', 'data', 'xml', 'sample02.xml')) }
+  let(:i_hash) { Nicc::XML.new(i_xml).parse }
+
   describe '#default_merge' do
-    let(:ext_xml)  { File.read(File.join('spec', 'data', 'xml', 'sample01.xml')) }
-    let(:ext_hash) { Nicc::XML.new(ext_xml).parse }
+    describe 'the default case that option has thread_type = "flat"' do
+      subject { response.default_merge }
+      let(:options)  { {} }
 
-    let(:i_xml)  { File.read(File.join('spec', 'data', 'xml', 'sample02.xml')) }
-    let(:i_hash) { Nicc::XML.new(i_xml).parse }
+      describe 'hash should have key & value' do
+        include_context 'The test of common elements'
+        include_context 'The test of common thread elements'
 
-    describe 'Actual data' do
-      describe 'the default case that option has thread_type = "flat"' do
-        let(:options)  { {} }
+        its(['description'])    { should == '「何故プレイ動画にしなかったのか」と何度も...<br />技術的に未熟な...<br /><br />' }
+        its(['view_counter'])   { should == '424445' }
+        its(['mylist_counter']) { should == '10128' }
+      end
+    end
 
-        describe 'hash should have key & value' do
-          subject { response.default_merge }
+    describe 'the case that option has thread_type = "layer"' do
+      let(:options)  { {thread_type: 'layer'} }
 
-          its(['video_id']) { should == 'sm3393520' }
-          its(['title']) { should == '【アイドルマスター＋麻雀】im@s 雀姫伝　第一話 前編' }
-          its(['description']) { should == '「何故プレイ動画にしなかったのか」と何度も...<br />技術的に未熟な...<br /><br />' }
-          its(['thumbnail_url']) { should == 'http://tn-skr1.smilevideo.jp/smile?i=3393520' }
-          its(['first_retrieve']) { should == '2008-05-22T07:23:27+09:00' }
-          its(['length']) { should == '11:07' }
-          its(['movie_type']) { should == 'mp4' }
-          its(['size_high']) { should == '39289914' }
-          its(['size_low']) { should == '33276193' }
-          its(['view_counter']) { should == '424445' }
-          its(['comment_num']) { should == '7948' }
-          its(['mylist_counter']) { should == '10128' }
-          its(['last_res_body']) { should == '赤木はるかwwww 今の...' }
-          its(['watch_url']) { should == 'http://www.nicovideo.jp/watch/sm3393520' }
-          its(['thumb_type']) { should == 'video' }
-          its(['embeddable']) { should =='1' }
-          its(['no_live_play']) { should == '0' }
-          its(['tags']) { should == {'domain'=>'jp', 'tag'=>['アイドルマスター', 'アイマス雀荘リンク', 'im@s架空戦記シリーズ', 'iM@s雀姫伝', 'RAP_']} }
-          its(['user_id']) { should == '43787' }
-          its(['user_nickname']) { should == 'RAP' }
-          its(['user_icon_url']) { should == 'http://usericon.nimg.jp/usericon/s/4/43787.jpg?1389728124' }
-          its(['deleted']) { should == '0' }
-          its(['length_in_seconds']) { should == '667' }
-          its(['upload_time']) { should == nil }
-          its(['default_thread']) { should == '1211408607' }
-          its(['option_flag_ichiba']) { should == '0' }
-          its(['option_flag_community']) { should == '0' }
-          its(['option_flag_domestic']) { should == '0' }
-          its(['option_flag_comment_type']) { should == '0' }
-          its(['option_flag_adult']) { should == '0' }
-          its(['option_flag_mobile']) { should == '0' }
-          its(['option_flag_economy_mp4']) { should == '1' }
-          its(['option_flag_middle_video']) { should == '0' }
-          its(['option_flag_mobile_ng_apple']) { should == '0' }
-          its(['public'])       { should == '1' }
-          its(['num_res'])      { should == '7948' }
-          its(['community_id']) { should == nil }
-          its(['thread_id'])           { should == '1211408607' }
-        end
+      describe 'hash should have key & value' do
+        subject { response.default_merge[:info] }
+
+        include_context 'The test of common elements'
+
+        its(['description'])    { should == '「何故プレイ動画にしなかったのか」と何度も...<br />技術的に未熟な...<br /><br />' }
+        its(['view_counter'])   { should == '424445' }
+        its(['mylist_counter']) { should == '10128' }
       end
 
-      describe 'the case that option has thread_type = "layer"' do
-        let(:options)  { {thread_type: 'layer'} }
+      describe 'hash should have key & value' do
+        subject { response.default_merge[:thread] }
+        include_context 'The test of common thread elements'
+      end
+    end
+  end
 
-        describe 'hash should have key & value' do
-          subject { response.default_merge[:info] }
+  describe '#reverse_merger' do
+    describe 'the default case that option has thread_type = "flat"' do
+      subject { response.reverse_merge }
+      let(:options)  { {} }
 
-          its(['video_id']) { should == 'sm3393520' }
-          its(['title']) { should == '【アイドルマスター＋麻雀】im@s 雀姫伝　第一話 前編' }
-          its(['description']) { should == '「何故プレイ動画にしなかったのか」と何度も...<br />技術的に未熟な...<br /><br />' }
-          its(['thumbnail_url']) { should == 'http://tn-skr1.smilevideo.jp/smile?i=3393520' }
-          its(['first_retrieve']) { should == '2008-05-22T07:23:27+09:00' }
-          its(['length']) { should == '11:07' }
-          its(['movie_type']) { should == 'mp4' }
-          its(['size_high']) { should == '39289914' }
-          its(['size_low']) { should == '33276193' }
-          its(['view_counter']) { should == '424445' }
-          its(['comment_num']) { should == '7948' }
-          its(['mylist_counter']) { should == '10128' }
-          its(['last_res_body']) { should == '赤木はるかwwww 今の...' }
-          its(['watch_url']) { should == 'http://www.nicovideo.jp/watch/sm3393520' }
-          its(['thumb_type']) { should == 'video' }
-          its(['embeddable']) { should =='1' }
-          its(['no_live_play']) { should == '0' }
-          its(['tags']) { should == {'domain'=>'jp', 'tag'=>['アイドルマスター', 'アイマス雀荘リンク', 'im@s架空戦記シリーズ', 'iM@s雀姫伝', 'RAP_']} }
-          its(['user_id']) { should == '43787' }
-          its(['user_nickname']) { should == 'RAP' }
-          its(['user_icon_url']) { should == 'http://usericon.nimg.jp/usericon/s/4/43787.jpg?1389728124' }
-          its(['deleted']) { should == '0' }
-          its(['length_in_seconds']) { should == '667' }
-          its(['upload_time']) { should == nil }
-          its(['default_thread']) { should == '1211408607' }
-          its(['option_flag_ichiba']) { should == '0' }
-          its(['option_flag_community']) { should == '0' }
-          its(['option_flag_domestic']) { should == '0' }
-          its(['option_flag_comment_type']) { should == '0' }
-          its(['option_flag_adult']) { should == '0' }
-          its(['option_flag_mobile']) { should == '0' }
-          its(['option_flag_economy_mp4']) { should == '1' }
-          its(['option_flag_middle_video']) { should == '0' }
-          its(['option_flag_mobile_ng_apple']) { should == '0' }
-        end
+      describe 'hash should have key & value' do
+        include_context 'The test of common elements'
+        include_context 'The test of common thread elements'
 
-        describe 'hash should have key & value' do
-          subject { response.default_merge[:thread] }
+        its(['description'])    { should == '「何故プレイ動画にしなかったのか」と何度も...' }
+        its(['view_counter'])   { should == '424401' }
+        its(['mylist_counter']) { should == '10124' }
+      end
+    end
 
-          its(['id'])           { should == '1211408607' }
-          its(['public'])       { should == '1' }
-          its(['num_res'])      { should == '7948' }
-          its(['community_id']) { should == nil }
-        end
+    describe 'the case that option has thread_type = "layer"' do
+      let(:options)  { {thread_type: 'layer'} }
+
+      describe 'hash should have key & value' do
+        subject { response.reverse_merge[:info] }
+
+        include_context 'The test of common elements'
+
+        its(['description'])    { should == '「何故プレイ動画にしなかったのか」と何度も...' }
+        its(['view_counter'])   { should == '424401' }
+        its(['mylist_counter']) { should == '10124' }
+      end
+
+      describe 'hash should have key & value' do
+        subject { response.default_merge[:thread] }
+        include_context 'The test of common thread elements'
       end
     end
   end
 end
+
