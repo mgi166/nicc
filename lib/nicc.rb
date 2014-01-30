0535b2436d4ed19f1ee92bf5472bfc05f4ac1f5b
mgi166 <skskoari@gmail.com>
2014-01-30 09:49:08 +0900

    * write a little code    * image final interface

--- lib/nicc.rb
+++ lib/nicc.rb
@@ -1,2 +1,14 @@
 module Nicc
+  class << self
+    def new(&block)
+    end
+
+    def get_info(id, options)
+    end
+  end
 end
+
+require 'nicc/ext_info'
+require 'nicc/i_info'
+require 'nicc/response'
+require 'nicc/xml'
--- spec/lib/nicc_spec.rb
+++ spec/lib/nicc_spec.rb
@@ -1,1 +1,4 @@
 require 'spec_helper'
+
+describe Nicc do
+end
